e7cf711f78717c3e5b3e6c304af41b0f4a6a55fc
myles-hash <mylesbillington@gmail.com>
2024-01-02 21:13:23 +0000

    impliment css, chnages to db and next q button

--- client/app.js
+++ client/app.js
@@ -1,22 +1,52 @@
 const form = document.getElementById("quiz-form");
+const quizContainer = document.getElementById("quiz-container");
 
 async function getQuiz() {
+
+
+
   const response = await fetch("http://localhost:8080/quiz");
   const qAndAs = await response.json();
 
-  qAndAs.forEach(function (qAndA) {
-    const question = document.getElementById("question");
-    const answer1 = document.getElementById("label1");
-    const answer2 = document.getElementById("label2");
-    const answer3 = document.getElementById("label3");
-    const answer4 = document.getElementById("label4");
+  qAndAs.forEach(function (qAndA, index) {
+    const questionContainer = document.createElement("div");
+    questionContainer.className = "questionContainerClass";
 
+    const image = document.createElement("img");
+    image.src = `${qAndA.image}`;
+
+    const question = document.createElement("h2");
     question.textContent = qAndA.question;
-    answer1.textContent = qAndA.answer1;
-    answer2.textContent = qAndA.answer2;
-    answer3.textContent = qAndA.answer3;
-    answer4.textContent = qAndA.answer4;
-  })
+
+    const answer1 = document.createElement("label");
+    answer1.innerHTML = `<input type="radio" name="answer" value="${qAndA.answer1}" /> <span>${qAndA.answer1}</span>`;
+
+    const answer2 = document.createElement("label");
+    answer2.innerHTML = `<input type="radio" name="answer" value="${qAndA.answer2}" /> <span>${qAndA.answer2}</span>`;
+
+    const answer3 = document.createElement("label");
+    answer3.innerHTML = `<input type="radio" name="answer" value="${qAndA.answer3}" /> <span>${qAndA.answer3}</span>`;
+
+    const answer4 = document.createElement("label");
+    answer4.innerHTML = `<input type="radio" name="answer" value="${qAndA.answer4}" /> <span>${qAndA.answer4}</span>`;
+
+    const nextButton = document.createElement("button");
+    nextButton.textContent = "Next Question";
+    nextButton.addEventListener("click", () => {
+    });
+
+
+    questionContainer.appendChild(question);
+    questionContainer.appendChild(image);
+    questionContainer.appendChild(answer1);
+    questionContainer.appendChild(answer2);
+    questionContainer.appendChild(answer3);
+    questionContainer.appendChild(answer4);
+    questionContainer.appendChild(nextButton);
+
+    quizContainer.appendChild(questionContainer);
+  });
+
 }
 
 getQuiz();
--- server/seed.js
+++ server/seed.js
@@ -8,70 +8,70 @@ CREATE TABLE IF NOT EXISTS quiztable (
     answer1 TEXT,
     answer2 TEXT,
     answer3 TEXT,
-    answer4 TEXT
+    answer4 TEXT,
+    image TEXT
 )
 `);
 
-db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4) VALUES (?, ?, ?, ?, ?)`).run(
-   "Will this quiz be good?",
-   "Yep",
-   "Yes",
-   "Maybe",
-   "No"
-  );
-
-  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4) VALUES (?, ?, ?, ?, ?)`).run(
+  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4, image) VALUES (?, ?, ?, ?, ?, ?)`).run(
     "1. Which of the following is filmmaker Michael Bay known for?",
     "Rom Com",
     "Noir",
     "Explosions",
-    "Westerns"
+    "Westerns",
+    "./CLIPS/1)Bay.jpg"
   );
 
-  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4) VALUES (?, ?, ?, ?, ?)`).run(
+  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4, image) VALUES (?, ?, ?, ?, ?, ?)`).run(
     "2. In 'The Princess Bride' (1987), what does the acronym 'R.O.U.S' stand for?",
     "Roads Over Useless Sidewalks",
     "Rodents of Unusual Size",
     "Really Oily Umami Salmon",
-    "Relegated Once Under Subversive"
+    "Relegated Once Under Subversive",
+    "./CLIPS/2)bride.jpg"
   );
 
-  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4) VALUES (?, ?, ?, ?, ?)`).run(
+  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4, image) VALUES (?, ?, ?, ?, ?, ?)`).run(
     "3. In 'Berserk, The Golden Age' (2012), what is the name of the group the protagonist joins?",
     "The Flaming Fists",
     "The Survey Corps",
     "The Phantom Troupe  ",
-    "The Band of the Hawk"
+    "The Band of the Hawk",
+    "./CLIPS/3)berserk.jpg"
   );
 
-  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4) VALUES (?, ?, ?, ?, ?)`).run(
+  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4, image) VALUES (?, ?, ?, ?, ?, ?)`).run(
     "4. In 'Krull' (1983), what is the cyclops' curse?",
     "Born with the knowledge of when they will die",
     "Born with only one eye",
     "Food turns to sand in their mouths",
-    "Hated and feared by all other living things on sight"
+    "Hated and feared by all other living things on sight",
+    "./CLIPS/4)krull.jpg"
   );
 
-  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4) VALUES (?, ?, ?, ?, ?)`).run(
+  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4, image) VALUES (?, ?, ?, ?, ?, ?)`).run(
     "5. In 'Parasite' (2019), who is still hiding in the house at the end of the film?",
     "The Cleaner",
     "The cat",
     "The father",
-    "Tim"
+    "Tim",
+    "./CLIPS/5)parasite.jpg"
   );
 
-  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4) VALUES (?, ?, ?, ?, ?)`).run(
+  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4, image) VALUES (?, ?, ?, ?, ?, ?)`).run(
     "6. What does Darth Vader say next?",
     "I am the farter",
     "My name is Martha",
     "I am your father",
-    "Would you rather"
+    "Would you rather",
+    "./CLIPS/6)Vader.mp4"
   );
 
-  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4) VALUES (?, ?, ?, ?, ?)`).run(
+  db.prepare(`INSERT INTO quiztable (question, answer1, answer2, answer3, answer4, image) VALUES (?, ?, ?, ?, ?, ?)`).run(
     "7. In 'Stalker' (1979), what is said to be at the center of 'the zone'?",
     "A room that grants your sincerest wish",
     "A monolith of stone that grants great riches",
     "The meaning of life",
-    "An escape"
+    "An escape",
+    "./CLIPS/7)stalker.jpg"
   );
