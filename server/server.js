e701ed0d7785892797dd3e380ab73b6862be1bcb
myles-hash <mylesbillington@gmail.com>
2024-01-05 13:43:33 +0000

    tried again

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static('client'));
 
 app.get("/quiz", (request, response) => {
   let qAndAs = [];
